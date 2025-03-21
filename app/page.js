27f545c8eedc0674722a3b12c09b3b614a79f642
priti <priti12bishnoi@gmail.com>
2025-03-21 11:50:45 +0530

    fdas

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Hero from "@/components/Hero";
+import MiddleCards from "@/components/MiddleCards";
+import Cards from "@/components/Cards";
+import Footer from "@/components/Footer";
+
+
+
+export default function Home () {
+  return (
+    <div className="w-full h-full ">
+      <Hero />
+      <MiddleCards />
+      <Cards />
+      <Footer />
+    </div>
+  );
+}
